3f7b32b2eced52e41e4b5359e9c0e48f0e65f2dd
doainn <daiphamgiabao@trulet.com>
2022-05-09 10:55:52 +0700

    refator

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'learn-rxjs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('learn-rxjs');
+    expect(app.title).toEqual('docs');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('learn-rxjs app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('docs app is running!');
   });
 });
